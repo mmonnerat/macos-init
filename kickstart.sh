7efcc3320bda315e9b5dbd41429ccafa8c295593
Cesar Caetano Neto <cesarcneto@gmail.com>
2019-11-16 17:47:30 +0100

    Set up basic Development environment The idea here is to automate installation of several pieces of softwares I use regularly

---
+++ kickstart.sh
@@ -0,0 +1,105 @@
+#!/usr/local/bin/bash
+set -e
+
+echo "😍 Kick starting your machine..."
+echo
+sleep 2
+
+INITDIR=$HOME/dev/projects/$USER/dotfiles
+# OSX Tweaks
+echo "🖱 Setting up OSX tweaks..."
+echo
+echo "This step will TRY to configure the following OSX settings:"
+echo " * Finder: Show Path bar"
+echo " * Finder: Show Hidden files"
+echo
+defaults write com.apple.finder ShowPathbar 1
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Generate SSH
+echo "🔶 Generating new SSH key..."
+echo
+ssh-keygen -t rsa -b 4096
+echo
+echo "Adding newly-created key to ssh-agent. You will be prompted for the password."
+eval "$(ssh-agent -s)"
+ssh-add -K ~/.ssh/id_rsa
+
+# Manual instruction suggested
+echo "🛑 Manual step before proceeding!"
+echo
+echo "To clone the init repositories the newly created SSH key needs to be uploaded to GitHub."
+pbcopy < ~/.ssh/id_rsa.pub
+echo "The public key (~/.ssh/id_rsa.pub) was copied to your clipboard."
+echo
+echo "  👾 Upload new SSH key to GH - https://github.com/settings/ssh/new"
+echo
+
+read -n 1 -s -r -p "Press any key to continue..."
+echo
+
+# Clone repos with dotfiles
+echo "🐛 Creating workspace and cloning general init project..."
+echo
+mkdir -p ~/dev/projects/$USER
+git clone git@github.com:cesarcneto/dotfiles.git $INITDIR
+cp $INITDIR/.ssh/config ~/.ssh/config
+
+# Install all needed software
+echo "⚙️ Installing all needed software from brew and brew cask..."
+echo
+brew install python3 go node nvm kubernetes-cli tfenv jq awscli hey derailed/k9s/k9s
+brew cask install firefox google-chrome visual-studio-code spectacle spotify intellij-idea postman docker gimp
+
+# Install RVM
+echo "Installing Ruby Version Manager - RVM ..."
+curl -sSL https://get.rvm.io | bash -s stable
+
+# Install SDK Man
+echo "Installing The Software Development Kit Manager - SDKMan ..."
+curl -s "https://get.sdkman.io" | bash
+
+# Install VS Code extensions
+echo "Installing VS Code extensions..."
+code --install-extension vscjava.vscode-java-pack
+code --install-extension ms-python.python
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension redhat.vscode-yaml
+code --install-extension arjun.swagger-viewer
+code --install-extension mauve.terraform
+code --install-extension vscjava.vscode-spring-initializr
+code --install-extension pivotal.vscode-spring-boot
+code --install-extension gabrielbb.vscode-lombok
+code --install-extension 42crunch.vscode-openapi
+echo
+
+# Setup dotfiles
+echo "🎫 Copying dotfiles..."
+echo
+echo "This step will:"
+echo " * Copy .zshrc to home"
+echo
+cp $INITDIR/.zshrc ~/
+source ~/.zshrc
+
+echo "🎉 All done!"
+echo
+echo "OSX tweaks to do manually:"
+echo " ⚙️  Displays > Resolution > More Space"
+echo " ⚙️  Keyboard > Input Sources > add U.S. International - PC > remove others"
+echo " ⚙️  Accessibility > Zoom > Use scroll gesture with modifier keys to zoom > ^ Control"
+echo " ⚙️  Security & Privacy > General > Require password immediately after sleep or screen saver begins"
+echo " ⚙️  Keyboard > Text > disable Correct spelling automatically and Capitalize words automatically"
+echo
+echo "Application configuration to do manually:"
+echo " 📅 Calendar:"
+echo "  ⚙️  Add Google accounts, configure refreshing for every 5min"
+echo
+echo " 👓 Spectacle:"
+echo "  ⚙️  Preferences > Launch Spectacle at Login"
+echo
+echo " 🌕 Firefox:"
+echo "  ⚙️  Set as default browser"
+echo "  ⚙️  Login to Firefox to synchronize extensions"
+echo
+echo "🌈 HAVE FUN! 🦄"
---
+++ pre_kickstart.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/bash
+set -e
+
+echo "😍 Kick starting your machine..."
+echo
+sleep 1
+
+INITDIR=$HOME/dev/projects/$USER/dotfiles
+
+# Install dev tools
+echo "🤓 Installing xcode CLI tools..."
+echo
+echo "This launches a GUI application and requires user confirmation."
+xcode-select --install
+
+read -n 1 -s -r -p "Press any key to continue..."
+echo
+
+# Install homebrew
+echo "🔨 Installing homebrew..."
+echo
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/cask-versions
+
+# Install proper terminal - iterm2 and oh-my-zsh
+echo "❤️ Installing iTerm2 and oh-my-zsh..."
+echo
+brew cask install iterm2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
